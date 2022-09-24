149a3e21601520a269e857a759a835dd1481b30b
anddoellinger <andreas.doellinger@live.de>
2022-09-24 16:54:27 -0500

    Testing .gitignore file

--- tools/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dates import Dates
-from .times import Times
-from .greetings import Greetings
-from .weathers import Weathers
--- utils/__init__.py
+++
@@ -1,2 +0,0 @@
-from .converter import lead_zero, unix_time_converter, speed_converter, gmt_time_converter, customize_description
-from .constants import weekdays, time_format, timezones, locations, greeting_summery
